c351846cda7d2d021b199abcc2acd669c85758ee
Mehemmed <LENOVA-NB@LENOVA>
2025-03-19 23:28:19 +0400

    filtered the products by category.

--- my-shop/src/State/FetchSlice.tsx
+++ my-shop/src/State/FetchSlice.tsx
@@ -7,19 +7,30 @@ export interface Product {
   description: string;
   price: number;
   thumbnail: string;
+  discountPercentage:number;
+  category:string;
+  brand:string
 }
 
 export interface CardState {
   data: Product[];
+  category:string[]
   isLoading: boolean;
   error: boolean;
+  selectedCategory:string;
 }
 
 const initialState: CardState = {
   data: [],
+  category:[],
   isLoading: false,
   error: false,
+  selectedCategory:'ALL'
 };
+
+
+
+
 
 // ✅ `fetch` istifadə edərək API məlumatlarını əldə edən `createAsyncThunk`
 export const fetchData = createAsyncThunk<Product[]>(
@@ -27,7 +38,7 @@ export const fetchData = createAsyncThunk<Product[]>(
   async (): Promise<Product[]> => {
     
     try {
-      const response = await fetch("https://dummyjson.com/products");
+      const response = await fetch("https://dummyjson.com/products?limit=12&skip=0");
       
       if (!response.ok) {
         throw new Error(`Xəta kodu: ${response.status}`);
@@ -46,7 +57,11 @@ export const fetchData = createAsyncThunk<Product[]>(
 export const FetchSlice = createSlice({
   name: "fetch",
   initialState,
-  reducers: {},
+  reducers: {
+    selectedCategories:(state,action)=>{
+      state.selectedCategory = action.payload
+    }
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchData.pending, (state) => {
       state.isLoading = true;
@@ -54,6 +69,9 @@ export const FetchSlice = createSlice({
     builder.addCase(fetchData.fulfilled, (state, action) => {
       state.isLoading = false;
       state.data = action.payload;
+      state.category = Array.from(new Set(action.payload.map(item=>item.category.toUpperCase())))
+      
+      
     });
     builder.addCase(fetchData.rejected, (state) => {
       state.isLoading = false;
@@ -61,5 +79,5 @@ export const FetchSlice = createSlice({
     });
   },
 });
-
+export const  {selectedCategories} = FetchSlice.actions
 export default FetchSlice.reducer;
--- my-shop/src/components/Cards.tsx
+++ my-shop/src/components/Cards.tsx
@@ -1,48 +1,68 @@
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "../State/store";
 import FavoriButton from "../layouts/FavoriButton";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { fetchData } from "../State/FetchSlice";
 
 const Cards = () => {
-  const dispatch = useDispatch<AppDispatch>()
-  const data = useSelector((state: RootState) => state.fetch);
-  useEffect(()=>{
-    
-    
-    dispatch(fetchData())
-  },[dispatch])
+  const dispatch = useDispatch<AppDispatch>();
+  const {  data,isLoading, error } = useSelector(
+    (state: RootState) => state.fetch
+  );
+  const selectedCategory = useSelector(
+    (state: RootState) => state.fetch.selectedCategory
+  );
 
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [data]);
   
-  
-
+  const getFilteredProducts = ()=>{
+     let filteredProducts = data
+    if(selectedCategory!=='ALL'){
+      filteredProducts = filteredProducts.filter(product=>product.category.toUpperCase()===selectedCategory)
+    }
+    else{
+      filteredProducts = data
+    }
+    return filteredProducts
+  }
+  const filteredProducts = getFilteredProducts()
   return (
     <>
-      {data.data.map((item)=>(
-        <div className="flex group relative cursor-pointer bg-white flex-col w-[20%] hover:shadow-[0_25px_29px_rgba(63,78,100,0.15)] transition-transform duration-300 ease-linear hover:border-2 hover:border-solid hover:border-[#EBEBEB]  hover:rounded-lg ">
-        <div className="w-full flex  ">
-          
-          <img
-            src={item.thumbnail}
-            alt={item.title}
-            className="w-full h-auto "
-          />
-  
-        </div>
-        <div className="absolute z-10 top-[15px] left-[15px] "><FavoriButton /></div>
-        <div className="flex flex-col px-[10px] items-center justify-center">
-          <Link to='/ProductInfo' className="text-[#1e1e27] hover:text-gray-500 flex items-center justify-center h-[55px] text-[18px]">{item.title}</Link>
-          <div className="flex  flex-row gap-[15px] justify-around items-center text-[14px]">
-              <p className="text-red-600 ">{item.price}</p>
-              <span className="text-[#b5aec4} line-through">Discount</span>
+      {filteredProducts.map((item) => (
+        <div
+          key={item.id}
+          className="flex group  relative cursor-pointer border-r-[#EBEBEB]  bg-white flex-col w-[20%] hover:shadow-[0_25px_29px_rgba(63,78,100,0.15)] transition-transform duration-300 ease-linear hover:border-2 hover:border-solid hover:border-[#EBEBEB]  hover:rounded-lg "
+        >
+          <div className="w-full flex  ">
+            <img
+              src={item.thumbnail}
+              alt={item.title}
+              className="w-full h-auto "
+            />
           </div>
+          <div className="absolute z-10 top-[15px] left-[15px] ">
+            <FavoriButton />
+          </div>
+          <div className="flex flex-col px-[10px] items-center justify-center">
+            <Link
+              to="/ProductInfo"
+              className="text-[#1e1e27] hover:text-gray-500 flex items-center justify-center h-[55px] text-[18px]"
+            >
+              {item.title}
+            </Link>
+            <div className="flex  flex-row gap-[15px] justify-around items-center text-[14px]">
+              <p className="text-red-600 ">${item.discountPercentage}</p>
+              <span className="text-[#b5aec4} line-through">${item.price}</span>
+            </div>
+          </div>
+
+          <button className="text-white mt-[20px] h-[40px] group-hover:bg-red-500 flex items-center justify-center hover:bg-red-400 transition duration-200 ease-linear w-full text-[12px] font-semibold">
+            ADD TO CARD
+          </button>
         </div>
-        
-        <button className="text-white mt-[20px] h-[40px] group-hover:bg-red-500 flex items-center justify-center hover:bg-red-400 transition duration-200 ease-linear w-full text-[12px] font-semibold">
-          ADD TO CARD
-        </button>
-      </div>
       ))}
     </>
   );
